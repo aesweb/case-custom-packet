179b90ea9865bc4a5ec1d2db053df199d9f1f260
aesweb <aes.sncr@gmail.com>
2024-06-20 10:52:17 +0300

    chore: Update Navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,26 @@
+import { FaRegUser } from 'react-icons/fa';
+import { FiShoppingCart } from 'react-icons/fi';
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="flex justify-center gap-24 items-center container mx-auto py-6 text-md text-gray-700">
+      <div>
+        <img src="logo.svg" alt="" />
+      </div>
+      <ul className="flex gap-10">
+        <li>Urunler</li>
+        <li>Biz Kimiz?</li>
+        <li>Bagis Kulturu</li>
+        <li>Regl Testi! </li>
+        <li>Kendi Paketini Olustur</li>
+      </ul>
+
+      <div className="gap-6 flex pl-16">
+        <FiShoppingCart size={20} />
+        <FaRegUser size={20} />
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
