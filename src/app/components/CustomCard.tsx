7aef444f35d2844df0f3d1b4c82c55ceace62cd5
aesweb <aes.sncr@gmail.com>
2024-06-20 15:35:43 +0300

    chore: Update CustomCard component layout

--- src/app/components/CustomCard.tsx
+++ src/app/components/CustomCard.tsx
@@ -1,14 +1,17 @@
 const CustomCard = () => {
   return (
-    <div className="flex flex-col gap-8">
-      <div className="flex justify-between items-center">
-        <div className="text-2xl">Kendi Paketini Oluştur</div>
-        <div className="text-md">Nasıl Çalışır?</div>
+    <div className="flex items-center flex-col gap-20">
+      <div className=" flex flex-col gap-8">
+        <div className="flex justify-between">
+          <div className="text-2xl">Kendi Paketini Oluştur</div>
+          <div className="text-md">Nasıl Çalışır?</div>
+        </div>
+        <div>
+          Tercih ve ihtiyaçların doğrultusunda seçeceğin ürünlerden ve
+          miktarlardan, sana özel bir paket oluşturalım.
+        </div>
       </div>
-      <div>
-        Tercih ve ihtiyaçların doğrultusunda seçeceğin ürünlerden ve
-        miktarlardan, sana özel bir paket oluşturalım.
-      </div>
+      <div>tabs</div>
     </div>
   );
 };
