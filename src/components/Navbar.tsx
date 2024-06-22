cc941ccb6795230b7d34cf0af36a85ebe5974852
aesweb <aes.sncr@gmail.com>
2024-06-22 15:36:27 +0300

    chore: Update dependencies to include @nextui-org/navbar and update components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,86 @@
-import { FaRegUser } from 'react-icons/fa';
-import { FiShoppingCart } from 'react-icons/fi';
+'use client';
+import React from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from '@nextui-org/react';
 
-function Navbar() {
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    'Ürünler',
+    'Biz Kimiz?',
+    'Bağış Kültürü',
+    'Blog',
+    'Regl Testi!',
+    'Kendi Paketini Oluştur',
+  ];
+
   return (
-    <header className="flex justify-center gap-24 items-center container mx-auto py-6 text-gray-700">
-      <div>
-        <img src="logo.svg" alt="" />
-      </div>
-      <ul className="flex gap-10">
-        <li>Urunler</li>
-        <li>Biz Kimiz</li>
-        <li>Bagis Kulturu</li>
-        <li>Regl Testi! </li>
-        <li>Kendi Paketini Olustur</li>
-      </ul>
+    <Navbar className="bg-[#F5F5F5]">
+      <NavbarBrand>
+        <img src="/logo.svg" alt="" />
+      </NavbarBrand>
+      <NavbarContent className="hidden md:flex gap-8" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#" className="text-sm">
+            Ürünler
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="#" color="foreground" className="text-sm">
+            Biz Kimiz?
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#" className="text-sm">
+            Bağış Kültürü
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#" className="text-sm">
+            Regl Testi!
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#" className="text-sm">
+            Kendi Paketini Oluştur
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Link href="#">
+            <img src="/basket.svg" alt="" />
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="#">
+            <img src="/user.svg" alt="" />
+          </Link>
+        </NavbarItem>
+        <NavbarMenuToggle
+          className="sm:hidden"
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+        />
+      </NavbarContent>
 
-      <div className="gap-6 flex pl-16">
-        <FiShoppingCart size={20} />
-        <FaRegUser size={20} />
-      </div>
-    </header>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link className="w-full" color="foreground" href="#" size="lg">
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   );
 }
-
-export default Navbar;
